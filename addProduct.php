66c0e99eb484e737167444558fe59cca36e22bda
SenaKMG <73621048+SenaKMG@users.noreply.github.com>
2021-12-12 10:49:05 -0500

    Add files via upload

--- addProduct.php
+++ addProduct.php
@@ -10,17 +10,17 @@
 
     <?php
 	
-		if(isset( $_POST['save']))
-		{
-			$ProductNumber= $_POST['pn'];
-			$ProductName= $_POST['name'];
-		    $ProductBrand= $_POST['brand'];
-		    $ProductCategory= $_POST['productcategory'];
-            $ProductQuantityPerUnit= $_POST['quant_per_product'];
-            $ProductUnitPrice = $_POST['uprice'];
-            $ProductAvailableStock= $_POST['productavailablestock'];
-            $ProductMaximumStock= $_POST['maxstock'];
-            $ProductDescription = $_POST['productdescription'];
+		//if(isset( $_POST['save']))
+		//{
+			$ProductNumber= $_POST["pn"];
+			$ProductName= $_POST["name"];
+		    $ProductBrand= $_POST["brand"];
+		    $ProductCategory= $_POST["product_category"];
+            $ProductQuantityPerUnit= $_POST["quant_per_product"];
+            $ProductUnitPrice = $_POST["uprice"];
+            $ProductAvailableStock= $_POST["productavailablestock"];
+            $ProductMaximumStock= $_POST["maxstock"];
+            $ProductDescription = $_POST["productdescription"];
 			
 			$itemstock_doc = new DOMDocument();
 			$itemstock_doc->preserveWhiteSpace = false;
@@ -32,7 +32,7 @@
             foreach (($itemstock_doc->childNodes)[0]->childNodes as $node){
 			
 				$concernednumber=($node->GetElementsByTagname("ProductNumber"))[0]->nodeValue;
-				if ($ProductNmber == $concernednumber)
+				if ($ProductNumber == $concernednumber)
 				{
 					echo "<script>alert('Product number already in use.'); document.location='P7.html'</script>";
 					exit();
@@ -41,47 +41,57 @@
 			}
 			
 			$new_item = $itemstock_doc->createElement("item");
+
 			$prodnumelem=$itemstock_doc->createElement("ProductNumber");
-			$prodnumelem->appendChild(createTextNode($ProductNumber));
+			$prodnumelem->appendChild($itemstock_doc->createTextNode($ProductNumber));
+			//$prodnumelem->appendChild($itemstock_doc->createTextNode("12334"));
 			$new_item->appendChild($prodnumelem);
 
 			$prodnamelem=$itemstock_doc->createElement("ProductName");
-			$prodnamelem->appendChild(createTextNode($ProductName));
-			$new_item->appendChild($prodnumelem);
+			$prodnamelem->appendChild($itemstock_doc->createTextNode($ProductName));
+			//$prodnamelem->appendChild($itemstock_doc->createTextNode("Testyprod"));
+			$new_item->appendChild($prodnamelem);
 
 			$prodbraelem=$itemstock_doc->createElement("ProductBrand");
-			$prodbraelem->appendChild(createTextNode($ProductBrand));
-			$new_item->appendChild($prodnumelem);
+			$prodbraelem->appendChild($itemstock_doc->createTextNode($ProductBrand));
+			//$prodbraelem->appendChild($itemstock_doc->createTextNode("MegaNoName"));
+			$new_item->appendChild($prodbraelem);
 
-			$prodnumelem=$itemstock_doc->createElement("ProductCategory");
-			$prodnumelem->appendChild(createTextNode($ProductCategory));
-			$new_item->appendChild($prodnumelem);
+			$prodcatelem=$itemstock_doc->createElement("ProductCategory");
+			$prodcatelem->appendChild($itemstock_doc->createTextNode($ProductCategory));
+			//$prodcatelem->appendChild($itemstock_doc->createTextNode("Dairy"));
+			$new_item->appendChild($prodcatelem);
 
 			$prodquaelem=$itemstock_doc->createElement("ProductQuantityPerUnit");
-			$prodqquaelem->appendChild(createTextNode($ProductQuantityPerUnit));
-			$new_item->appendChild($prodnumelem);
+			$prodquaelem->appendChild($itemstock_doc->createTextNode($ProductQuantityPerUnit));
+			//$prodquaelem->appendChild($itemstock_doc->createTextNode("3241"));
+			$new_item->appendChild($prodquaelem);
 
-			$produnitelem=$itemstock_doc->createElement("ProductUnitPrice");
-			$produnielem->appendChild(createTextNode($ProductUnitPrice));
-			$new_item->appendChild($prodnumelem);
+			$produnielem=$itemstock_doc->createElement("ProductUnitPrice");
+			$produnielem->appendChild($itemstock_doc->createTextNode($ProductUnitPrice));
+			//$produnielem->appendChild($itemstock_doc->createTextNode("12"));
+			$new_item->appendChild($produnielem);
 
 			$prodavaelem=$itemstock_doc->createElement("ProductAvailableStock");
-			$prodavaelem->appendChild(createTextNode($ProductAvailableStock));
-			$new_item->appendChild($prodnumelem);
+			$prodavaelem->appendChild($itemstock_doc->createTextNode($ProductAvailableStock));
+			//$prodavaelem->appendChild($itemstock_doc->createTextNode("31"));
+			$new_item->appendChild($prodavaelem);
 
 			$prodmaxelem=$itemstock_doc->createElement("ProductMaximumStock");
-			$prodmaxelem->appendChild(createTextNode($ProductMaximumStock));
-			$new_item->appendChild($prodnumelem);
+			$prodmaxelem->appendChild($itemstock_doc->createTextNode($ProductMaximumStock));
+			//$prodmaxelem->appendChild($itemstock_doc->createTextNode("70"));
+			$new_item->appendChild($prodmaxelem);
 
 			$proddeselem=$itemstock_doc->createElement("ProductDescription");
-			$proddeselem->appendChild(createTextNode($ProductDescription));
-			$new_item->appendChild($prodnumelem);
+			$proddeselem->appendChild($itemstock_doc->createTextNode($ProductDescription));
+			//$proddeselem->appendChild($itemstock_doc->createTextNode("I love myself"));
+			$new_item->appendChild($proddeselem);
 
 			($itemstock_doc->childNodes)[0]->appendChild($new_item);
 			$itemstock_doc->save("itemstock.xml");
 
 			
-		}
+		//}
 
 		echo "<script> location.href='P7.php'; </script>";
         exit;
